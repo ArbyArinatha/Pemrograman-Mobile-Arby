0fb20cd82c64f78977eb8a8b2548f8790696f5ff
Arby Arinatha <61810143+ArbyArinatha@users.noreply.github.com>
2020-12-17 19:21:21 +0800

    pertemuan mandiri 11
    pertemuan 11

---
+++ Pertemuan mandiri 11/screens/index.js
@@ -0,0 +1,33 @@
+import LoginScreen from './Login';
+import SignupScreen from './Signup';
+import DetailsScreen from './Details';
+import TabScreen from './Menu/Tab';
+
+module.exports = [
+
+  {
+    name: 'Login',
+    component: LoginScreen,
+    options: { headerShown: false },
+  },
+  {
+    name: 'Signup',
+    component: SignupScreen,
+    options: { headerShown: false },
+  },
+  {
+    name: 'Tab',
+    component: TabScreen,
+    options: { headerShown: false },
+  },
+  {
+    name: 'Details',
+    component: DetailsScreen,
+    options: { headerShown: false },
+  },
+  {
+    name: 'Order',
+    component: OrdersScreen,
+    options: { headerShown: false },
+  },
+];
